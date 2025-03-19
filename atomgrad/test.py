289b9a97b605edc30543638340b496d33e2f490f
Raki Delmoro <rakidelmoro@gmail.com>
2025-03-19 13:55:54 +0800

    Compare it with torch

--- atomgrad/test.py
+++ atomgrad/test.py
@@ -1,29 +1,38 @@
 import atom
 import torch
+import numpy as np
 
-def test_sanity():
-    x = atom.Tensor(-4.0, requires_grad=True)
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).relu()
-    y = h + q + q * x
-    y.backward()
-    xmg, ymg = x, y
+def nn_forward_test():
+    gen_x = np.random.randn(2, 10)
+    gen_w = np.random.randn(5, 10)
+    gen_b = np.random.randn(5)
+    gen_y = np.random.randn(2, 5)
 
-    x = torch.tensor([-4.0], dtype=torch.double, requires_grad=True)
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).relu()
-    y = h + q + q * x
-    y.backward()
-    xpt, ypt = x, y
+    atom_x = atom.tensor(gen_x, requires_grad=True)
+    atom_y = atom.tensor(gen_y, requires_grad=True)
 
-    # forward pass satisfy
-    assert ymg.data == ypt.data.item()
-    # backward pass satisfy
-    assert xmg.grad.data == xpt.grad.item()
+    atom_w = atom.tensor(gen_w, requires_grad=True)
+    atom_b = atom.tensor(gen_b, requires_grad=True)
+    pre_act = atom.matmul(atom_x, atom_w)
+    a_act = atom.add(pre_act, atom_b)
+    grad = (a_act['data'] - atom_y['data']) / atom_y['data'].shape[-1] # MSELoss
+    atom.backward(a_act, grad)
 
-    print(ymg.data, ypt.data.item())
-    print(xmg.grad.data, xpt.grad.item())
+    torch_x = torch.tensor(gen_x, requires_grad=True, dtype=torch.float32)
+    torch_y = torch.tensor(gen_y, requires_grad=True, dtype=torch.float32)
 
-test_sanity()
+    torch_w = torch.tensor(gen_w, requires_grad=True, dtype=torch.float32)
+    torch_b = torch.tensor(gen_b, requires_grad=True, dtype=torch.float32)
+    t_act = torch.matmul(torch_x, torch_w.T) + torch_b
+    loss = torch.nn.MSELoss().forward(t_act, torch_y)
+    loss.backward()
+
+    # forward pass satisfaction
+    print(a_act['data'].tolist())
+    print(t_act.data.tolist())
+
+    # backward pass satisfaction
+    print(atom_w['grad'])
+    print(torch_w.grad)
+
+nn_forward_test()
