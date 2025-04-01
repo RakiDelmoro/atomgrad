18cd262cf0c32dad9ad9e05ef68cd310fc84ff84
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-01 13:17:57 +0800

    atom activations function

--- atomgrad/activations_fn/atom_activations.py
+++ atomgrad/activations_fn/atom_activations.py
@@ -3,11 +3,19 @@ import atomgrad.activations_fn.activations_ops as act
 def relu():
     def forward(atom_tensor): return act.relu_ops(atom_tensor)
 
+    return forward
+
 def softmax():
     def forward(atom_tensor): return act.softmax_ops(atom_tensor)
+
+    return forward
 
 def leaky_relu():
     def forward(atom_tensor): return act.leaky_relu_ops(atom_tensor)
 
+    return forward
+
 def tanh():
     def forward(atom_tensor): return act.tanh_ops(atom_tensor)
+
+    return forward
