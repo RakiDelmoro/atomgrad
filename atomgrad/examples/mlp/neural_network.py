a9d9ab30613d8fc3ffeac12ef3121d477027fecc
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-02 09:09:01 +0800

    quick fix

--- atomgrad/examples/mlp/neural_network.py
+++ atomgrad/examples/mlp/neural_network.py
@@ -1,9 +1,9 @@
+import random
+import numpy as np
 import atomgrad.atom as atom
 import atomgrad.nn_ops as nn_ops
-import numpy as np
-import atomgrad.activation_ops as act_ops
 import atomgrad.optimizer as opt
-import random
+import atomgrad.activations_fn.atom_activations as act_ops
 from features import GREEN, RED, RESET
 
 def mlp():
@@ -22,9 +22,13 @@ def mlp():
         return linear_2_out
     
     def cross_entropy_loss(prediction, expected):
-        prediction_probs = atom.softmax(prediction)
-        grad = (prediction_probs - expected.numpy())
-        avg_loss = -np.mean(np.sum(expected.numpy() * np.log(prediction_probs), axis=-1))
+        def log_softmax(prediction):
+            shifted = prediction['data'] - np.max(prediction['data'], axis=-1, keepdims=True)
+            return shifted - np.log(np.sum(np.exp(shifted), axis=-1, keepdims=True))
+
+        prediction_probs = act_ops.softmax()(prediction)
+        grad = (prediction_probs['data'] - expected.numpy())
+        avg_loss = -np.mean(np.sum(expected.numpy() * log_softmax(prediction), axis=-1))
 
         return avg_loss, grad
 
@@ -38,9 +42,8 @@ def mlp():
             opt.zero_grad(parameters)
             atom.backward(model_prediction, gradients)
             opt.step(input_batched['data'].shape[0], parameters)
-
+            print(avg_loss)
             each_batch_loss.append(avg_loss)
-            # print(avg_loss)
 
         return np.mean(np.array(each_batch_loss))
     
@@ -52,7 +55,7 @@ def mlp():
             batched_image = atom.tensor(batched_image, requires_grad=True)
             batched_label = batched_label.numpy()
 
-            model_pred_probabilities = forward(batched_image)['data']
+            model_pred_probabilities = act_ops.softmax()(forward(batched_image))['data']
             batch_accuracy = (model_pred_probabilities.argmax(axis=-1) == batched_label.argmax(axis=-1)).mean()
             for each in range(len(batched_label)//10):
                 model_prediction = model_pred_probabilities[each].argmax()
