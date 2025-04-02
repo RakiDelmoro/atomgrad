cf32ce5d2b039a28104644dd361ce4c391a135b4
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-02 14:25:24 +0800

    test file atom ops

---
+++ atomgrad/tests/test_ops.py
@@ -0,0 +1,23 @@
+import ops
+import torch
+import numpy as np
+
+x1 = np.random.randn(1000, 10000)
+x2 = np.random.randn(10000, 10000)
+
+x1 = np.random.randn(10, 1000, 1000)
+x2 = np.random.randn(10, 1000, 1000)
+
+def test_matmul_ops():
+    pass
+
+def test_add_ops():
+    pass
+
+def test_mean_ops():
+    pass
+
+def test_sub_ops():
+    pass
+
+
