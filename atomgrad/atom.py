023c1eee2b547ec41b6243195f1b05dec1235447
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-02 22:26:15 +0800

    Update atom tensors ops

--- atomgrad/atom.py
+++ atomgrad/atom.py
@@ -1,17 +1,16 @@
 import numpy as np
+import atomgrad.ops as ops
 
 def tensor(data, requires_grad=False):
     """Create a tensor with data and gradient tracking."""
     return {'data': np.array(data, dtype=np.float32), 'shape': np.array(data).shape, 'grad': np.zeros_like(data) if requires_grad else None, 'requires_grad': requires_grad, 'grad_fn': None, 'depends_on': []}
 
 '''TENSOR OPS'''
-def add(x1, x2):
-    if isinstance(x1, np.ndarray): x1 = tensor(x1)
-    if isinstance(x2, np.ndarray): x2 = tensor(x2)
 
+def add(x1, x2):
     requires_grad = x1['requires_grad'] or x2['requires_grad']
 
-    result = tensor(x1['data'] + x2['data'], requires_grad)
+    result = tensor(ops.add(x1['data'], x2['data']), requires_grad)
     result['depends_on'] = [x1, x2]
 
     def grad_fn(grad):
@@ -33,8 +32,9 @@ def add(x1, x2):
 def sub(x1, x2):
     requires_grad = x1['requires_grad'] or x2['requires_grad']
 
-    return tensor(x1['data'] - x2['data'], requires_grad)
+    return tensor(ops.sub(x1['data'], x2['data']), requires_grad)
 
+# TODO: Transfer the operation in ops.py file
 def broadcasted_mul(x1, x2):
     requires_grad = x1['requires_grad'] or x2['requires_grad']
 
@@ -63,11 +63,8 @@ def broadcasted_mul(x1, x2):
     return result
 
 def mul(x1, x2):
-    if isinstance(x1, np.ndarray): x1 = tensor(x1)
-    if isinstance(x2, np.ndarray): x2 = tensor(x2)
-
     requires_grad = x1['requires_grad'] or x2['requires_grad']
-    result = tensor(x1['data'] * x2['data'], requires_grad)
+    result = tensor(ops.mul(x1['data'], x2['data']), requires_grad)
     result['depends_on'] = [x1, x2]
 
     def grad_fn(grad):
@@ -81,12 +78,9 @@ def mul(x1, x2):
     return result
 
 def matmul(x1, x2):
-    if isinstance(x1, np.ndarray): x1 = tensor(x1)
-    if isinstance(x2, np.ndarray): x2 = tensor(x2)
-
     requires_grad = x1['requires_grad'] or x2['requires_grad']
 
-    result = tensor(np.matmul(x1['data'], x2['data'].T), requires_grad)
+    result = tensor(ops.matmul(x1['data'], x2['data'].T), requires_grad)
     result['depends_on'] = [x1, x2]
 
     def grad_fn(grad):
@@ -98,84 +92,10 @@ def matmul(x1, x2):
 
     return result
 
-def matmul_3d(x1, x2):
-    requires_grad = x1['requires_grad'] or x2['requires_grad']
-    
-    x1_data = x1['data']
-    x2_data = x2['data']
-    
-    x1_shape = x1_data.shape
-    x2_shape = x2_data.shape
-
-    # Check dimensions
-    is_x1_3d = len(x1_shape) == 3
-    is_x2_3d = len(x2_shape) == 3
-    
-    # x1 @ x2 (2d shape size)
-    if not is_x1_3d and not is_x2_3d:
-        result_data = np.matmul(x1_data, x2_data.T)
-
-    # x1(3d shape size) @ x2(2d shape size) 
-    elif is_x1_3d and not is_x2_3d:
-        if len(x2_shape) == 1: result_data = np.matmul(x1_data, x2_data)
-        elif x2_shape[0] == x1_shape[0]:
-            result_data = np.zeros((x1_shape[0], x1_shape[1]))
-            for i in range(x1_shape[0]): result_data[i] = np.matmul(x1_data[i], x2_data[i])
-        else:
-            result_data = np.zeros((x1_shape[0], x1_shape[1], x2_shape[0]))
-            for i in range(x1_shape[0]): result_data[i] = np.matmul(x1_data[i], x2_data.T)
-
-    # x1(2d shape size) @ x2(3d shape size)
-    elif not is_x1_3d and is_x2_3d:
-        if len(x1_shape) == 1:
-            result_data = np.zeros((x2_shape[0], x2_shape[2]))
-            for i in range(x2_shape[0]): result_data[i] = np.matmul(x1_data, x2_data[i])
-        elif x1_shape[0] == x2_shape[0]:
-            result_data = np.zeros((x2_shape[0], x2_shape[2]))
-            for i in range(x2_shape[0]): result_data[i] = np.matmul(x1_data[i], x2_data[i])
-        else:
-            result_data = np.zeros((x2_shape[0], x1_shape[0], x2_shape[2]))
-            for i in range(x2_shape[0]): result_data[i] = np.matmul(x1_data, x2_data[i])
-    
-    # x1 @ x2 (3d shape size)
-    else:
-        # Both 3D case - batch dimensions should match
-        if x1_shape[0] != x2_shape[0]:
-            raise ValueError("Batch dimensions must match for 3D-3D matmul")
-        
-        # Case: (batch, m, n) @ (batch, n, p) -> (batch, m, p)
-        result_data = np.zeros((x1_shape[0], x1_shape[1], x2_shape[2]))
-        for i in range(x1_shape[0]):
-            result_data[i] = np.matmul(x1_data[i], x2_data[i])
-    
-    result = tensor(result_data, requires_grad)
-    result['depends_on'] = [x1, x2]
-    
-    def grad_fn(grad):
-        # Zero grad
-        # x1['grad'] = np.zeros_like(x1['data'])
-        # x2['grad'] = np.zeros_like(x1['data'])
-
-        if is_x1_3d and not is_x2_3d and len(x2_shape) == 2 and x2_shape[0] == x1_shape[0]:
-            if x1['requires_grad']:
-                for i in range(x1_shape[0]): x1['grad'][i] += np.outer(grad[i], x2_data[i])
-            if x2['requires_grad']:
-                for i in range(x2_shape[0]): x2['grad'][i] += np.matmul(grad[i], x1_data[i])
-        else:
-            if x1['requires_grad']: 
-                if not is_x1_3d: x1['grad'] += grad @ x2_data
-                else:
-                    for i in range(x1_shape[0]): x1['grad'][i] += grad[i][:, np.newaxis] @ x2_data[i][np.newaxis, :]
-            if x2['requires_grad']:
-                if not is_x2_3d: x2['grad'] += grad.T @ x1_data
-                else:
-                    for i in range(x2_shape[0]): x2['grad'][i] += x1_data[i].T @ grad[i]
-
-    result['grad_fn'] = grad_fn
-    return result
-
-def sum_tensors(x):
-    result = tensor(sum([t for t in x['data']]), requires_grad=True)
+#TODO: Run test for this
+def sum_tensors(x, axis=0):
+    list_atom_data = [atom_tensor['data'] for atom_tensor in x]
+    result = tensor(ops.sum_arrays(list_atom_data, axis), requires_grad=True)
     result['depends_on'] = [x]
 
     def grad_fn(grad):
@@ -185,8 +105,10 @@ def sum_tensors(x):
     result['grad_fn'] = grad_fn
     return result
 
+#TODO: Run test for this
 def mean_tensor(x, axis=0):
-    result = tensor(np.stack([t['data'] for t in x]).mean(axis=axis), requires_grad=True)
+    list_atom_data = [atom_tensor['data'] for atom_tensor in x]
+    result = tensor(ops.mean_arrays(list_atom_data, axis), requires_grad=True)
     result['depends_on'] = [i for i in x]
 
     def grad_fn(grad):
@@ -195,6 +117,7 @@ def mean_tensor(x, axis=0):
 
     result['grad_fn'] = grad_fn
     return result
+
 
 def build_topo(nodes):
     """Build topological order starting from the given node."""
@@ -215,6 +138,7 @@ def build_topo(nodes):
             topo.append(node)
     visit(nodes)
     return topo
+
 
 def backward(atom_tensor, grad=None):
     """Compute gradients via reverse-mode autodiff."""
