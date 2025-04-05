98999d78b51f63ceec112e0e874c9172e1999b2f
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-05 10:33:33 +0800

    loss function

---
+++ atomgrad/loss_fn/losses_ops.py
@@ -0,0 +1,15 @@
+import numpy as np
+import atomgrad.activations_fn.atom_activations as act_ops
+
+def _cross_entropy_loss(prediction, expected):
+    act_probabilities = act_ops.softmax()
+    log_softmax_act = act_ops.log_softmax()
+
+    prediction_probs = act_probabilities(prediction)
+    grad = (prediction_probs['data'] - expected.numpy())
+    avg_loss = -np.mean(np.sum(expected.numpy() * log_softmax_act(prediction), axis=-1))
+
+    return avg_loss, grad
+
+def _mean_squared_loss(prediction, expected):
+    pass
