46cfe66271edc37e96d0cdd468d2968eb4f68d58
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-06 23:08:20 +0800

    add more functionality

--- atomgrad/ops.py
+++ atomgrad/ops.py
@@ -1,14 +1,22 @@
 import numpy as np
 
-def add(x1: np.ndarray | int, x2: np.ndarray | int): return x1 + x2
+def _add(x1: np.ndarray | int, x2: np.ndarray | int): return x1 + x2
 
-def sub(x1: np.ndarray | int, x2: np.ndarray | int): return x1 - x2
+def _sub(x1: np.ndarray | int, x2: np.ndarray | int): return x1 - x2
 
-def mul(x1: np.ndarray | int, x2: np.ndarray | int): return x1 * x2
+def _mul(x1: np.ndarray | int, x2: np.ndarray | int): return x1 * x2
 
-def div(x1: np.ndarray | int, x2: np.ndarray | int): return x1 * x2**-1
+def _div(x1: np.ndarray | int, x2: np.ndarray | int): return x1 * x2**-1
 
-def matmul(x1: np.ndarray, x2: np.ndarray):
+def _mean_arrays(x: list, axis=0):
+    x = np.stack(x, axis=0)
+    return np.mean(x, axis)
+
+def _sum_arrays(x: list, axis=0):
+    x = np.stack(x, axis=0)
+    return np.sum(x, axis)
+
+def _matmul(x1: np.ndarray, x2: np.ndarray):
     # Check num dim
     x1_shape = x1.shape
     x2_shape = x2.shape
@@ -18,11 +26,11 @@ def matmul(x1: np.ndarray, x2: np.ndarray):
     is_x2_3d = len(x2_shape) == 3
 
     if is_x1_3d or is_x2_3d:
-        return matmul_3d(x1, x2.T)
+        return _matmul_3d(x1, x2.T)
 
     return np.matmul(x1, x2)
 
-def matmul_3d(x1: np.ndarray, x2: np.ndarray):
+def _matmul_3d(x1: np.ndarray, x2: np.ndarray):
     # Check num dim
     x1_shape = x1.shape
     x2_shape = x2.shape
@@ -68,10 +76,5 @@ def matmul_3d(x1: np.ndarray, x2: np.ndarray):
 
     return result_data
 
-def mean_arrays(x: list, axis=0):
-    x = np.stack(x, axis=0)
-    return np.mean(x, axis)
-
-def sum_arrays(x: list, axis=0):
-    x = np.stack(x, axis=0)
-    return np.sum(x, axis)
+def _broadcast_mul():
+    pass
