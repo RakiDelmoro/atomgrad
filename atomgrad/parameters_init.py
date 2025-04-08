b9e7408fab48bce7b057393c581190dca434182e
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-08 14:20:43 +0000

    add init parameters for gpu ops

--- atomgrad/parameters_init.py
+++ atomgrad/parameters_init.py
@@ -1,15 +1,15 @@
 import math
 import numpy as np
-import atomgrad.atom as atom
 import atomgrad.init as atom_init
+from atomgrad.tensor import atom
 
 '''Collection of parameters initializations'''
 
-def atom_kaiming_init(input_size, output_size):
+def atom_kaiming_init(input_size, output_size, device):
     gen_w = np.empty(shape=(output_size, input_size))
     weights = atom_init.kaiming_uniform(gen_w, a=math.sqrt(5))
     fan_in, _ = atom_init._calculate_fan_in_and_fan_out(weights)
     bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
     bias = np.random.uniform(-bound, bound, size=(output_size,))
 
-    return [atom.tensor(weights, requires_grad=True), atom.tensor(bias, requires_grad=True)]
+    return [atom(weights, requires_grad=True, device=device), atom(bias, requires_grad=True, device=device)]
