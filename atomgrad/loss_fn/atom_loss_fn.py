494395665f670b4cce12bee1871f96f7dc13a3c0
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-08 14:22:25 +0000

    loss fn have gpu ops

--- atomgrad/loss_fn/atom_loss_fn.py
+++ atomgrad/loss_fn/atom_loss_fn.py
@@ -1,7 +1,8 @@
 import atomgrad.loss_fn.losses_ops as loss
 
 def cross_entropy_loss():
-    def forward(prediction, expected): return loss._cross_entropy_loss(prediction, expected)
+    def forward(prediction, expected):
+        return loss._cross_entropy_loss(prediction, expected)
 
     return forward
 
