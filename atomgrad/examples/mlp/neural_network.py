aa54365c94649fbee0233ec6ae3d0d5b704c0ccc
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-08 14:10:29 +0000

    atom ops with gpu

--- atomgrad/examples/mlp/neural_network.py
+++ atomgrad/examples/mlp/neural_network.py
@@ -1,10 +1,11 @@
 import random
 import numpy as np
-import atomgrad.atom as atom
+import atomgrad.atom as a
 import atomgrad.nn_ops as nn_ops
 import atomgrad.optimizer as optimizer
 import atomgrad.loss_fn.atom_loss_fn as loss_ops
 import atomgrad.activations_fn.atom_activations as act_ops
+from atomgrad.tensor import atom
 
 # Colors
 RED = '\033[31m'
@@ -13,9 +14,9 @@ GREEN = '\033[32m'
 
 def mlp():
     parameters = [] 
-    linear_1, params_1 = nn_ops.linear_layer(784, 2000)
+    linear_1, params_1 = nn_ops.linear_layer(784, 2000, device='cuda')
     activation = act_ops.relu()
-    linear_2, params_2 = nn_ops.linear_layer(2000, 10)
+    linear_2, params_2 = nn_ops.linear_layer(2000, 10, device='cuda')
 
     parameters.extend(params_1)
     parameters.extend(params_2)
@@ -31,17 +32,16 @@ def mlp():
 
     def training_phase(dataloader):
         each_batch_loss = []
-        step, zero_grad = optimizer.adam(parameters, lr=0.001)
+        step, zero_grad = optimizer.adam(parameters, lr=0.001, device='cuda')
         for input_batched, label_batched in dataloader:
-            input_batched = atom.tensor(input_batched, requires_grad=True)
+            input_batched = atom(input_batched, requires_grad=True, device='cuda')
             model_prediction = forward(input_batched)
             avg_loss, gradients = loss_fn(model_prediction, label_batched)
 
             zero_grad(parameters)
-            atom.backward(model_prediction, gradients)
+            a.backward(model_prediction, gradients)
             step(input_batched['data'].shape[0])
-            print(avg_loss)
-            each_batch_loss.append(avg_loss)
+            each_batch_loss.append(avg_loss.item())
 
         return np.mean(np.array(each_batch_loss))
 
@@ -50,17 +50,17 @@ def mlp():
         correctness = []
         wrongness = []
         for i, (batched_image, batched_label) in enumerate(dataloader):
-            batched_image = atom.tensor(batched_image, requires_grad=True)
-            batched_label = batched_label.numpy()
+            batched_image = atom(batched_image, requires_grad=True, device='cuda')
+            batched_label = atom(batched_label.numpy(), device='cuda')
 
             model_pred_probabilities = act_ops.softmax()(forward(batched_image))['data']
-            batch_accuracy = (model_pred_probabilities.argmax(axis=-1) == batched_label.argmax(axis=-1)).mean()
-            for each in range(len(batched_label)//10):
+            batch_accuracy = (model_pred_probabilities.argmax(axis=-1) == batched_label['data'].argmax(axis=-1)).mean()
+            for each in range(len(batched_label['data'])//10):
                 model_prediction = model_pred_probabilities[each].argmax()
-                if model_prediction == batched_label[each].argmax(axis=-1): correctness.append((model_prediction.item(), batched_label[each].argmax(axis=-1).item()))
-                else: wrongness.append((model_prediction.item(), batched_label[each].argmax(axis=-1).item()))
+                if model_prediction == batched_label['data'][each].argmax(axis=-1): correctness.append((model_prediction.item(), batched_label['data'][each].argmax(axis=-1).item()))
+                else: wrongness.append((model_prediction.item(), batched_label['data'][each].argmax(axis=-1).item()))
             print(f'Number of samples: {i+1}\r', end='', flush=True)
-            accuracy.append(np.mean(batch_accuracy))
+            accuracy.append(np.mean(batch_accuracy.item()))
         random.shuffle(correctness)
         random.shuffle(wrongness)
         print(f'{GREEN}Model Correct Predictions{RESET}')
--- atomgrad/ops.py
+++ atomgrad/ops.py
@@ -1,12 +1,41 @@
+import cupy as cp
 import numpy as np
+import atomgrad.tensor as tensor
 
-def _add(x1: np.ndarray | int, x2: np.ndarray | int): return x1 + x2
+def _add(x1, x2):
+    x1_device = x1['device']
+    x2_device = x2['device']
+    assert x1['device'] == x2['device'], f'x1 and x2 has to be same device: {x1_device}, {x2_device}'
 
-def _sub(x1: np.ndarray | int, x2: np.ndarray | int): return x1 - x2
+    result = x1['data'] + x2['data']
+    return result
 
-def _mul(x1: np.ndarray | int, x2: np.ndarray | int): return x1 * x2
+def _sub(x1, x2):
+    x1_device = x1['device']
+    x2_device = x2['device']
 
-def _div(x1: np.ndarray | int, x2: np.ndarray | int): return x1 * x2**-1
+    assert x1['device'] == x2['device'], f'x1 and x2 has to be same device: {x1_device}, {x2_device}'
+    
+    result = x1['data'] - x2['data']
+    return result
+
+def _mul(x1, x2):
+    x1_device = x1['device']
+    x2_device = x2['device']
+
+    assert x1['device'] == x2['device'], f'x1 and x2 has to be same device: {x1_device}, {x2_device}'
+
+    result = x1['data'] * x2['data']
+    return result
+
+def _div(x1, x2):
+    x1_device = x1['device']
+    x2_device = x2['device']
+
+    assert x1['device'] == x2['device'], f'x1 and x2 has to be same device: {x1_device}, {x2_device}'
+
+    result = x1['data'] * x2['data']**-1
+    return result
 
 def _mean_arrays(x: list, axis=0):
     x = np.stack(x, axis=0)
@@ -16,24 +45,37 @@ def _sum_arrays(x: list, axis=0):
     x = np.stack(x, axis=0)
     return np.sum(x, axis)
 
-def _matmul(x1: np.ndarray, x2: np.ndarray):
+def _matmul(x1, x2):
+    x1_device = x1['device']
+    x2_device = x2['device']
+
+    assert x1['device'] == x2['device'], f'x1 and x2 has to be same device: {x1_device}, {x2_device}'
+
     # Check num dim
-    x1_shape = x1.shape
-    x2_shape = x2.shape
+    x1_shape = x1['data'].shape
+    x2_shape = x2['data'].shape
 
     # Check dimensions
     is_x1_3d = len(x1_shape) == 3
     is_x2_3d = len(x2_shape) == 3
 
     if is_x1_3d or is_x2_3d:
-        return _matmul_3d(x1, x2.T)
+        result = _matmul_3d(x1, x2['data'])
+    else:
+        result = np.matmul(x1['data'], x2['data'].T) if x1['device'] == 'cpu' else cp.matmul(x1['data'], x2['data'].T)
 
-    return np.matmul(x1, x2)
+    return result
 
-def _matmul_3d(x1: np.ndarray, x2: np.ndarray):
+def _matmul_3d(x1, x2):
+    x1_device = x1['device']
+    x2_device = x2['device']
+
+    assert x1['device'] == x2['device'], f'x1 and x2 has to be same device: {x1_device}, {x2_device}'
+
+    device = x1['device']
     # Check num dim
-    x1_shape = x1.shape
-    x2_shape = x2.shape
+    x1_shape = x1['data'].shape
+    x2_shape = x2['data'].shape
 
     # Check dimensions
     is_x1_3d = len(x1_shape) == 3
@@ -42,37 +84,40 @@ def _matmul_3d(x1: np.ndarray, x2: np.ndarray):
     if not is_x1_3d and not is_x2_3d:
         raise ValueError(f"Should have 3d shape {x1.shape} {x2.shape}")
 
-    if not is_x1_3d and not is_x2_3d: result_data = np.matmul(x1, x2.T)
-
-    elif is_x1_3d and not is_x2_3d:
+    if is_x1_3d and not is_x2_3d:
         if len(x2_shape) == 1: result_data = np.matmul(x1, x2)
         elif x2_shape[0] == x1_shape[0]:
-            result_data = np.zeros((x1_shape[0], x1_shape[1]))
-            for i in range(x1_shape[0]): result_data[i] = np.matmul(x1[i], x2[i])
+            if device == 'cpu': result_data = np.zeros((x1_shape[0], x1_shape[1]))
+            else: result_data = cp.zeros((x1_shape[0], x1_shape[1]))
+            for i in range(x1_shape[0]): result_data[i] = np.matmul(x1['data'][i], x2['data'][i]) if device == 'cpu' else cp.matmul(x1['data'][i], x2['data'][i])
         else:
-            result_data = np.zeros((x1_shape[0], x1_shape[1], x2_shape[0]))
-            for i in range(x1_shape[0]): result_data[i] = np.matmul(x1[i], x2.T)
+            if device == 'cpu':
+                result_data = np.zeros((x1_shape[0], x1_shape[1], x2_shape[0]))
+            else: result_data = cp.zeros((x1_shape[0], x1_shape[1], x2_shape[0]))
+            for i in range(x1_shape[0]): result_data[i] = np.matmul(x1['data'][i], x2['data'].T) if device == 'cpu' else cp.matmul(x1['data'][i], x2['data'].T)
 
     elif not is_x1_3d and is_x2_3d:
         if len(x1_shape) == 1:
-            result_data = np.zeros((x2_shape[0], x2_shape[2]))
-            for i in range(x2_shape[0]): result_data[i] = np.matmul(x1, x2[i])
+            if device == 'cpu': result_data = np.zeros((x2_shape[0], x2_shape[2]))
+            else: result_data = cp.zeros((x2_shape[0], x2_shape[2]))
+            for i in range(x2_shape[0]): result_data[i] = np.matmul(x1['data'], x2['data'][i]) if device == 'cpu' else cp.matmul(x1['data'], x2['data'][i])
         elif x1_shape[0] == x2_shape[0]:
-            result_data = np.zeros((x2_shape[0], x2_shape[2]))
-            for i in range(x2_shape[0]): result_data[i] = np.matmul(x1[i], x2[i])
+            if device == 'cpu': result_data = np.zeros((x2_shape[0], x2_shape[2]))
+            else: result_data = cp.zeros((x2_shape[0], x2_shape[2]))
+            for i in range(x2_shape[0]): result_data[i] = np.matmul(x1['data'][i], x2['data'][i]) if device == 'cpu' else cp.matmul(x1['data'][i], x2['data'][i])
         else:
-            result_data = np.zeros((x2_shape[0], x1_shape[0], x2_shape[2]))
-            for i in range(x2_shape[0]): result_data[i] = np.matmul(x1, x2[i])
-
+            if device == 'cpu': result_data = np.zeros((x2_shape[0], x1_shape[0], x2_shape[2]))
+            else: result_data = cp.zeros((x2_shape[0], x1_shape[0], x2_shape[2]))
+            for i in range(x2_shape[0]): result_data[i] = np.matmul(x1['data'], x2['data'][i]) if device == 'cpu' else cp.matmul(x1['data'], x2['data'[i]])
     else:
         # Both 3D case - batch dimensions should match
-        if x1_shape[0] != x2_shape[0]:
-            raise ValueError("Batch size must match for 3D-3D matmul")
+        if x1_shape[0] != x2_shape[0]: raise ValueError("Batch size must match for 3D-3D matmul")
 
         # Case: (batch, m, n) @ (batch, n, p) -> (batch, m, p)
-        result_data = np.zeros((x1_shape[0], x1_shape[1], x2_shape[2]))
+        if device == 'cpu': result_data = np.zeros((x1_shape[0], x1_shape[1], x2_shape[2]))
+        else: result_data = cp.zeros((x1_shape[0], x1_shape[1], x2_shape[2]))
         for i in range(x1_shape[0]):
-            result_data[i] = np.matmul(x1[i], x2[i])
+            result_data[i] = np.matmul(x1[i]['data'], x2[i]['data']) if device == 'cpu' else cp.matmul(x1[i]['data'], x2[i]['data'])
 
     return result_data
 
