f9081602aca2e5223a3e848f7b4e6ebc837b9c7f
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-09 08:19:09 +0000

    can run on cpu/cuda

--- classic_mlp.py
+++ classic_mlp.py
@@ -1,25 +1,28 @@
 import pickle
-from dataset.utils import image_data_batching
+import tqdm 
+from dataset.utils import mnist_dataloader
 from atomgrad.examples.mlp.neural_network import mlp
 
 def main_runner():
-    MAX_EPOCHS = 20
+    MAX_EPOCHS = 100
     IMAGE_HEIGHT = 28
     IMAGE_WIDTH = 28
+    BATCH_SIZE = 4096
 
     with open('./dataset/mnist.pkl', 'rb') as f: ((train_images, train_labels), (test_images, test_labels), _) = pickle.load(f, encoding='latin1')
     assert train_images.shape[0] == train_labels.shape[0]
     assert test_images.shape[0] == test_labels.shape[0]
     assert train_images.shape[1] == test_images.shape[1] == IMAGE_HEIGHT*IMAGE_WIDTH
 
-    train_runner, test_runner = mlp()
+    train_runner, test_runner = mlp(device='cuda')
 
-    for epoch in range(MAX_EPOCHS):
-        train_loader = image_data_batching(train_images, train_labels, batch_size=128, shuffle=True)
-        test_loader = image_data_batching(test_images, test_labels, batch_size=128, shuffle=True)
+    for _ in (t := tqdm.trange(MAX_EPOCHS)):
+        train_loader = mnist_dataloader(train_images, train_labels, batch_size=BATCH_SIZE, shuffle=True)
+        test_loader = mnist_dataloader(test_images, test_labels, batch_size=BATCH_SIZE, shuffle=True)
 
         train_loss = train_runner(train_loader)
         accuracy = test_runner(test_loader)
-        print(f"EPOCH: {epoch+1} Loss: {train_loss} Accuracy: {accuracy}")
+
+        t.set_description(f'Loss: {train_loss:.4f} Accuracy: {accuracy:.4f}')
 
 main_runner()
