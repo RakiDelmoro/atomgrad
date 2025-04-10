97fed3637765a9640b86e2c8496c24bb2b66a8a2
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-10 02:49:13 +0000

    test deriv softmax

---
+++ atomgrad/cpu/test.py
@@ -0,0 +1,46 @@
+import atom
+import torch
+import activations_fn.activations as act
+
+# Colors
+RED = '\033[31m'
+RESET = '\033[0m'
+GREEN = '\033[32m'
+UNDERLINE = "\033[4m"
+
+def deriv_softmax():
+    # Init
+    logits = torch.randn(3, 3)
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+    grad = torch.randn_like(probs)
+
+    # TORCH deriv softmax
+    t_logits = logits.clone().detach().requires_grad_(True)
+    t_probs = torch.nn.functional.softmax(t_logits, dim=-1)
+    t_loss_scalar = (t_probs * grad).sum()
+    t_loss_scalar.backward()
+    t_calculated_grad = t_logits.grad
+
+    # ATOM deriv softmax
+    a_logits = atom.cpu_tensor(logits.numpy(), requires_grad=True)
+    a_probs = act.softmax()(a_logits)
+    a_grad = atom.cpu_tensor(grad.numpy())['data']
+    # call backward in atom
+    a_probs['grad_fn'](a_grad)
+    a_calculated_grad = a_logits['grad']
+
+    # for double checking
+    print(t_probs)
+    print(a_probs['data'])
+
+    satisfied = torch.allclose(torch.tensor(a_calculated_grad), t_calculated_grad)
+
+    if satisfied:
+        print(f"softmax derivative --->>> {GREEN}PASSED{RESET}")
+    else:
+        print(f"softmax derivative --->>> {RED}FAILED{RESET}")
+
+def deriv_3d_matmul():
+    pass
+
+deriv_softmax()
