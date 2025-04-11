79ec1896c6f353cf960682360e5f0a4d33deaf13
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-11 09:57:37 +0000

    test with forward and backward pass

--- test_transformer.py
+++ test_transformer.py
@@ -2,9 +2,19 @@ import cupy as cp
 import atomgrad.atom as atom
 from atomgrad.examples.transformer.neural_network import transformer
 
-model = transformer(embedding_dim=512, vocab_size=5)
+def test_runner():
+    model, model_params = transformer(embedding_dim=512, vocab_size=5, num_transformer_blocks=4)
 
-x_test = atom.tensor(cp.random.randint(low=0, high=5, size=(1, 10)), device='cuda')
-print(model(x_test)['data'].shape)
+    x_test = atom.tensor(cp.random.randint(low=0, high=5, size=(1, 10)), device='cuda')
+    pred_model, emb = model(x_test)
+    grad = cp.random.randn(1, 10, 5)
 
-# TODO: Train with real texts
+    atom.backward(pred_model, grad)
+
+    print(emb['grad'])
+    print(model_params[-1]['grad'])
+
+    # Backpropagation FIXED
+    #TODO: Feed me with real data!
+
+test_runner()
