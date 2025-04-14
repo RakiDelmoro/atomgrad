704b12e06da866a90e57c987e51c47355c10ec98
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-14 13:01:15 +0000

    Add torch transformer runner

--- test_transformer.py
+++ test_transformer.py
@@ -4,7 +4,10 @@ import atomgrad.atom as atom
 import atomgrad.cuda.atom as cuda_atom
 import atomgrad.cuda.optimizer as cuda_opt
 import atomgrad.cuda.loss_fn.loss_fn_nn as loss_fn
-from atomgrad.examples.transformer.neural_network import transformer
+from atomgrad.examples.transformer.neural_network import atom_transformer
+
+import torch
+from atomgrad.torch_examples.transformer.neural_network import TorchTransformer
 
 BATCH_SIZE = 32
 BLOCK_SIZE = 256
@@ -24,7 +27,7 @@ def get_batch(data):
 
     return cuda_atom.cuda_tensor(context, requires_grad=True), cuda_atom.cuda_tensor(next_context, requires_grad=True)
 
-def evaluate_iterations(model, train_data, test_data):
+def atom_evaluate_iterations(model, train_data, test_data):
     train_losses = cp.zeros(EVAL_ITERS)
     test_losses = cp.zeros(EVAL_ITERS)
 
@@ -44,7 +47,31 @@ def evaluate_iterations(model, train_data, test_data):
 
     return cp.mean(train_losses), cp.mean(test_losses)
 
-def test_runner():
+@torch.no_grad()
+def torch_evaluate_iterations(model, train_data, test_data):
+    out = {}
+    model.eval()
+
+    train_losses = torch.zeros(EVAL_ITERS)
+    test_losses = torch.zeros(EVAL_ITERS)
+    for k in range(EVAL_ITERS):
+        x_train, y_train = get_batch(train_data)
+        x_test, y_test = get_batch(test_data)
+
+        x_train, y_train = torch.tensor(cp.asnumpy(x_train['data']), dtype=torch.long, device='cuda'), torch.tensor(cp.asnumpy(y_train['data']), dtype=torch.long, device='cuda')
+        x_test, y_test = torch.tensor(cp.asnumpy(x_test['data']), dtype=torch.long, device='cuda'), torch.tensor(cp.asnumpy(y_test['data']), dtype=torch.long, device='cuda')
+        
+        train_logits, train_loss = model(x_train, y_train)
+        test_logits, test_loss = model(x_test, y_test)
+        
+        train_losses[k] = train_loss.item()
+        test_losses[k] = test_loss.item()
+
+        print(f'EVAL: {k}', end='\r', flush=True)
+    model.train()
+    return torch.mean(train_losses), torch.mean(test_losses)
+
+def atom_test_runner():
     with open('./dataset/shakespeare.txt', 'r', encoding='utf-8') as f: text = f.read()
 
     chars = sorted(list(set(text)))
@@ -62,21 +89,60 @@ def test_runner():
     train_data = data[:split]
     test_data = data[split:]
 
-    model, parameters = transformer(embedding_dim=EMBEDDING_DIM, vocab_size=vocab_size, num_transformer_blocks=6, num_attn_heads=6)
+    model, parameters = atom_transformer(embedding_dim=EMBEDDING_DIM, vocab_size=vocab_size, num_transformer_blocks=6, num_attn_heads=6)
     loss_func = loss_fn.cross_entropy_loss()
     adam_step, adam_zero_grad = cuda_opt.adam_w(parameters, LEARNING_RATE)
 
     for epoch in range(MAX_EPOCHS):
-        if epoch % EVAL_ITERVAL == 0 or epoch == MAX_EPOCHS -1:
-            print('EVALUATING>>>')
-            train_loss, test_loss = evaluate_iterations(model, train_data, test_data)
-            print(f"step {iter}: train loss {train_loss:.4f}, val loss {test_loss:.4f}")
+    #     if epoch % EVAL_ITERVAL == 0 or epoch == MAX_EPOCHS -1:
+    #         print('EVALUATING>>>')
+    #         train_loss, test_loss = evaluate_iterations(model, train_data, test_data)
+    #         print(f"step {iter}: train loss {train_loss:.4f}, val loss {test_loss:.4f}")
 
         x_batched, y_batched = get_batch(train_data)
         model_pred, loss, grad = model(x_batched, y_batched)
-
+        print(f'EPOCH: {epoch} LOSS: {loss}')
         adam_zero_grad(parameters)
-        atom.backward(model_pred, grad)
-        adam_step(x_batched['shape'][0])
+        atom.backward(model_pred, grad.reshape(BATCH_SIZE, BLOCK_SIZE, vocab_size))
+        adam_step(x_batched['shape'][0], x_batched['shape'][1])
 
-test_runner()
+def torch_test_runner():
+    with open('./dataset/shakespeare.txt', 'r', encoding='utf-8') as f: text = f.read()
+
+    chars = sorted(list(set(text)))
+    vocab_size = len(chars)
+    
+    string_to_int = {ch:i for i, ch in enumerate(chars)}
+    int_to_string = {i:ch for i, ch in enumerate(chars)}
+
+    encode_text = lambda text: [string_to_int[ch] for ch in text]
+    decode_text = lambda ints: ''.join([int_to_string[i] for i in ints])
+
+    # Train and test splits
+    data = np.array(encode_text(text), dtype=np.longlong)
+    split = int(0.9*len(data)) # 90% train rest test
+    train_data = data[:split]
+    test_data = data[split:]
+
+    model = TorchTransformer()
+    loss_func = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+
+    for epoch in range(MAX_EPOCHS):
+        # if epoch % EVAL_ITERVAL == 0 or epoch == MAX_EPOCHS -1:
+        #     print('EVALUATING>>>')
+        #     train_loss, test_loss = torch_evaluate_iterations(model, train_data, test_data)
+        #     print(f"step {epoch}: train loss {train_loss:.4f}, val loss {test_loss:.4f}")
+
+        x_batched, y_batched = get_batch(train_data)
+        x_batched, y_batched = torch.tensor(cp.asnumpy(x_batched['data']), dtype=torch.long, device='cuda'), torch.tensor(cp.asnumpy(y_batched['data']), dtype=torch.long, device='cuda')
+        model_pred, loss = model(x_batched, y_batched)
+
+        # print(f'EPOCH: {epoch} LOSS: {loss}', end='\r', flush=True)
+        print(f'EPOCH: {epoch} LOSS: {loss}')
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()        
+
+# torch_test_runner()
+atom_test_runner()
