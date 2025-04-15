f79048fce25af02a1662efd32cb2f9eb39afcd9e
Raki Delmoro <rakidelmoro@gmail.com>
2025-04-15 13:32:17 +0000

    Atom transformer use for test

--- atomgrad/examples/transformer/neural_network.py
+++ atomgrad/examples/transformer/neural_network.py
@@ -7,9 +7,11 @@ import atomgrad.cuda.loss_fn.loss_fn_nn as loss_fn
 # Test run
 VOCAB_SIZE = 10
 SEQ_LEN = 3
+EMBEDDING_DIM = 10
 
 # Actual Run
-# VOCAB_SIZE = 384
+# VOCAB_SIZE = 65
+# EMBEDDING_DIM = 384
 # SEQ_LEN = 256
 
 def char_embeddings(char_params=None, pos_params=None):
@@ -18,8 +20,8 @@ def char_embeddings(char_params=None, pos_params=None):
         # char_embedding, char_emb_params = cuda_ops.embeddings(num_embeddings=VOCAB_SIZE, embedding_dim=VOCAB_SIZE, parameters=char_params)
         # pos_embedding, pos_emb_params = cuda_ops.embeddings(num_embeddings=SEQ_LEN, embedding_dim=VOCAB_SIZE, parameters=pos_params)
     # else:
-    char_embedding, char_emb_params = cuda_ops.embeddings(num_embeddings=VOCAB_SIZE, embedding_dim=VOCAB_SIZE, parameters=char_params) #if char_params is None else char_params
-    pos_embedding, pos_emb_params = cuda_ops.embeddings(num_embeddings=SEQ_LEN, embedding_dim=VOCAB_SIZE, parameters=pos_params) #if pos_params is None else pos_params
+    char_embedding, char_emb_params = cuda_ops.embeddings(num_embeddings=VOCAB_SIZE, embedding_dim=EMBEDDING_DIM, parameters=char_params) #if char_params is None else char_params
+    pos_embedding, pos_emb_params = cuda_ops.embeddings(num_embeddings=SEQ_LEN, embedding_dim=EMBEDDING_DIM, parameters=pos_params) #if pos_params is None else pos_params
 
     trainable_parameters.extend([char_emb_params])
     trainable_parameters.extend([pos_emb_params])
@@ -36,8 +38,7 @@ def char_embeddings(char_params=None, pos_params=None):
     return forward, trainable_parameters
 
 def attention_layer(head_size, embedding_dim, query_params=None, key_params=None, value_params=None):
-    block_size = 256
-    tril = cp.tril(cp.ones((block_size, block_size)))
+    tril = cp.tril(cp.ones((SEQ_LEN, SEQ_LEN)))
     learnable_parameters = []
 
     attn_dropout = cuda_ops.dropout(p=0.2)
@@ -89,25 +90,27 @@ def multi_head_attention_layer(num_heads, head_size, embedding_dim, attn_heads_p
         learnable_parameters.extend(attn_params)
 
     dropout_proj = cuda_ops.dropout(p=0.2)
-    out_projection, attn_heads_params = cuda_ops.linear_layer(head_size * num_heads, embedding_dim, linear_proj_params)
-    learnable_parameters.extend(attn_heads_params)
+    out_projection, attn_proj_params = cuda_ops.linear_layer(head_size * num_heads, embedding_dim, linear_proj_params)
+    learnable_parameters.extend(attn_proj_params)
 
     def forward(data):
         attention_heads_outputs = [block(data) for block in attention_blocks]
         concatenated_attn_heads = cuda_atom.concatenate(attention_heads_outputs, axis=-1)
-        out_dropout = concatenated_attn_heads
-        # out_dropout = dropout_proj(concatenated_attn_heads)
+        # out_dropout = concatenated_attn_heads
+        out = out_projection(concatenated_attn_heads)
 
-        return out_projection(out_dropout)
+        # return dropout_proj(out)
+        return out
 
     return forward, learnable_parameters
 
 def feedforward(embedding_dim, linear_1_params=None, linear_2_params=None, layer_norm_params=None):
+# def feedforward(embedding_dim):
     learnable_parameters = []
 
-    linear_1, linear_1_params = cuda_ops.linear_layer(embedding_dim, embedding_dim * 4, parameters=linear_1_params)
+    linear_1, linear_1_params = cuda_ops.linear_layer(embedding_dim, embedding_dim * 4, linear_1_params)
     activation_fn = atom_act.relu()
-    linear_2, linear_2_params = cuda_ops.linear_layer(embedding_dim * 4, embedding_dim, parameters=linear_2_params)
+    linear_2, linear_2_params = cuda_ops.linear_layer(embedding_dim * 4, embedding_dim, linear_2_params)
 
     ff_dropout = cuda_ops.dropout(p=0.2)
     ff_layer_norm, norm_params = cuda_ops.layer_norm(embedding_dim, params=layer_norm_params)
@@ -130,17 +133,17 @@ def feedforward(embedding_dim, linear_1_params=None, linear_2_params=None, layer
     
     return forward, learnable_parameters
 
-def transformer_block(num_attn_heads, embedding_dim):
+def transformer_block(num_attn_heads, embedding_dim, layer_norm_params, attn_heads_params, attn_out_proj, feedforward_params, ff_layer_norm):
+# def transformer_block(num_attn_heads, embedding_dim):
     learnable_parameters = []
 
     head_size =  embedding_dim // num_attn_heads
-    multi_heads_attention, mha_params = multi_head_attention_layer(num_attn_heads, head_size, embedding_dim)
+
+    layer_norm1, layer_norm1_params = cuda_ops.layer_norm(embedding_dim, params=layer_norm_params)
+    multi_heads_attention, mha_params = multi_head_attention_layer(num_attn_heads, head_size, embedding_dim, attn_heads_params=attn_heads_params, linear_proj_params=attn_out_proj)
 
     # FeedForward
-    feedforward_net, feedforward_params = feedforward(embedding_dim)
-
-    # Gradients is successfully propagte in layernorms parameters
-    layer_norm1, layer_norm1_params = cuda_ops.layer_norm(embedding_dim)
+    feedforward_net, feedforward_params = feedforward(embedding_dim, feedforward_params[0], feedforward_params[1], ff_layer_norm)
 
     learnable_parameters.extend(layer_norm1_params)
     learnable_parameters.extend(mha_params)
@@ -150,20 +153,39 @@ def transformer_block(num_attn_heads, embedding_dim):
         # Residual Connection
         attention_head_output = multi_heads_attention(layer_norm1(data))
         mha_output = cuda_atom.add(data, attention_head_output)
+        out = feedforward_net(mha_output)
 
-        ff_out = feedforward_net(mha_output)
-
-        # Residual Connection
-        out = cuda_atom.add(mha_output, ff_out)
         return out
 
     return forward, learnable_parameters
 
-def atom_transformer(num_transformer_blocks=4, block_size=256, vocab_size=10, embedding_dim=128, num_attn_heads=4):
+def transformer_blocks(num_transformer_blocks, num_attn_heads, embedding_dim, input_layer_norm_params=None, attn_heads_params=None, attn_out_params=None, feed_forward_params=None, feed_forward_layer_norm_params=None):
+# def transformer_blocks(num_transformer_blocks, num_attn_heads, embedding_dim):
+    learnable_parameters = []
+
+    transformer_blocks = []
+    for block in range(num_transformer_blocks):
+        transformer_layer, params = transformer_block(num_attn_heads=num_attn_heads, embedding_dim=embedding_dim, layer_norm_params=input_layer_norm_params[block], attn_heads_params=attn_heads_params[block], attn_out_proj=attn_out_params[block], feedforward_params=feed_forward_params[block], ff_layer_norm=feed_forward_layer_norm_params[block])
+        # transformer_layer, params = transformer_block(num_attn_heads=num_attn_heads, embedding_dim=embedding_dim)
+        transformer_blocks.append(transformer_layer)
+        learnable_parameters.extend(params)
+
+    def forward(data):
+        embeddings = data
+        for each in transformer_blocks:
+            embeddings = each(embeddings)
+
+        return embeddings
+    
+    return forward, learnable_parameters
+
+def atom_transformer(num_transformer_blocks=6, vocab_size=10, embedding_dim=128, num_attn_heads=6):
     learnable_parameters = []
 
     tok_embeddings, parameters = char_embeddings()
     learnable_parameters.extend(parameters)
+
+    # blocks, transformer_params = transformer_blocks(num_transformer_blocks, num_attn_heads, embedding_dim)
 
     transformer_blocks = []
     for _ in range(num_transformer_blocks):
@@ -173,19 +195,17 @@ def atom_transformer(num_transformer_blocks=4, block_size=256, vocab_size=10, em
 
     layer_norm, layer_norm_params = cuda_ops.layer_norm(embedding_dim)
     linear_classifier, classifier_params = cuda_ops.linear_layer(embedding_dim, vocab_size)
-
     learnable_parameters.extend(layer_norm_params)
     learnable_parameters.extend(classifier_params)
 
     def forward(input_data, target=None):
         B, T = input_data['shape']
 
-        tok_and_pos_embeddings = tok_embeddings(input_data)
+        embeddings = tok_embeddings(input_data)
         for each in transformer_blocks:
-            embeddings = each(tok_and_pos_embeddings)
-            tok_and_pos_embeddings = embeddings
+            embeddings = each(embeddings)
     
-        out = linear_classifier(layer_norm(tok_and_pos_embeddings))
+        out = linear_classifier(layer_norm(embeddings))
 
         if target is None:
             loss = None
