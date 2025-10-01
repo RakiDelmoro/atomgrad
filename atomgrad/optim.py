60ab466ce5097eb9f66f4e8030cf75b413155838
Raki Delmoro <rakidelmoro@gmail.com>
2025-10-01 06:56:11 +0000

    Update

--- atomgrad/optim.py
+++ atomgrad/optim.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cupy as cp
-from tensor import atom
+from atomgrad.tensor import atom
 
 def SGD(parameters, lr=0.001):
     def update(scale):
@@ -16,29 +16,27 @@ def SGD(parameters, lr=0.001):
 
 def Adam(parameters, lr=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
 
-    def update(scale):
+    def update():
         # Initialize timestep counter for bias correction
         if not hasattr(update, 't'): update.t = 0
 
         update.t += 1
 
         for param in parameters:
-            grad = param.grad / scale
-
             if not hasattr(param, 'moment'):
                 param.moment = atom.zeros_like(param, param.device)
             if not hasattr(param, 'velocity'):
                 param.velocity = atom.zeros_like(param, param.device)
 
-            param.moment.data = beta1 * param.moment.data + (1 - beta1) * grad.data
-            param.velocity.data = beta2 * param.velocity.data + (1 - beta2) * (grad.data**2)
+            param.moment.data = beta1 * param.moment.data + (1 - beta1) * param.grad.data
+            param.velocity.data = beta2 * param.velocity.data + (1 - beta2) * (param.grad.data**2)
 
             m_hat = param.moment.data / (1 - beta1**update.t)
             v_hat = param.velocity.data / (1 - beta2**update.t)
             v_hat = np.sqrt(v_hat) if param.device == 'cpu' else cp.sqrt(v_hat)
 
             param.data -= lr * (m_hat / (v_hat + epsilon))
-    
+
     def zero_grad():
         for param in parameters:
             param.grad = atom.zeros_like(param, param.device)
