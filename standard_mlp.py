704b1157168c3061c351d95d33e4f4da6ce18f04
Raki Delmoro <rakidelmoro@gmail.com>
2025-10-07 09:25:42 +0000

    Scale

--- standard_mlp.py
+++ standard_mlp.py
@@ -3,9 +3,9 @@ import torch
 class StandardMLP(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.lin_1 = torch.nn.Linear(784, 380, device='cuda')
+        self.lin_1 = torch.nn.Linear(784, 2000, device='cuda')
         self.activation = torch.nn.ReLU()
-        self.lin_2 = torch.nn.Linear(380, 10, device='cuda')
+        self.lin_2 = torch.nn.Linear(2000, 10, device='cuda')
 
     def forward(self, x):
         lin1_out = self.lin_1(x)
